29a9c6fb69b02296427e9a5efc1474e974bc0a8c
David Michael II <knowdavidmichael@gmail.com>
2022-04-23 00:48:07 +0100

    feat: add BaseProvider and StyletronProvider to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import React from 'react';
+import { Provider as StyletronProvider } from 'styletron-react';
+import '../styles/globals.css';
+import { BaseProvider, LightTheme } from 'baseui';
+import { debug, styletron } from '../styletron';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
+      <BaseProvider theme={LightTheme}>
+        <Component {...pageProps} />
+      </BaseProvider>
+    </StyletronProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
