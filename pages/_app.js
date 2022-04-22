2f28424793f63ee6a7f36937577e4b252a2aac1d
David Michael II <knowdavidmichael@gmail.com>
2022-04-23 00:53:04 +0100

    feat: add CustomTheme to MyApp component

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Provider as StyletronProvider } from 'styletron-react';
-import { BaseProvider, LightTheme } from 'baseui';
+import { BaseProvider } from 'baseui';
 import { debug, styletron } from '../styletron';
+import CustomTheme from '../theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
-      <BaseProvider theme={LightTheme}>
+      <BaseProvider theme={CustomTheme}>
         <Component {...pageProps} />
       </BaseProvider>
     </StyletronProvider>
