98d139f3959a6bccfc414a416cbd38562dd75f76
David Michael II <knowdavidmichael@gmail.com>
2022-04-23 01:23:48 +0100

    feat: changestp layout

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Heading, HeadingLevel } from 'baseui/heading';
 
 function Layout({ children }) {
   return (
     <>
       {/* Seo Component */}
 
-      {/* Header Component */}
+      <header>
+        <HeadingLevel>
+          <Heading>NextJS</Heading>
+        </HeadingLevel>
+      </header>
 
       <main>{children}</main>
     </>
