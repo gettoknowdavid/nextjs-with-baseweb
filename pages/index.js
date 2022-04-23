0eb9d1bd462965c5a8f366c33d99aade2e10dabe
David Michael II <knowdavidmichael@gmail.com>
2022-04-23 01:01:20 +0100

    feat: changes to home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Head from 'next/head';
 import {
   Button, KIND, SHAPE, SIZE,
 } from 'baseui/button';
 import { Heading, HeadingLevel } from 'baseui/heading';
-import { Cell, Grid } from 'baseui/layout-grid';
-import { ParagraphSmall } from 'baseui/typography';
-import Head from 'next/head';
+import {
+  ALIGNMENT, BEHAVIOR, Cell, Grid,
+} from 'baseui/layout-grid';
+import { ParagraphLarge } from 'baseui/typography';
 
 function Home() {
   return (
@@ -15,18 +17,15 @@ function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Grid>
+      <Grid align={ALIGNMENT.center} behavior={BEHAVIOR.fluid}>
         <Cell span={12}>
           <HeadingLevel>
-            <Heading>Boilerplate Next + BaseWeb </Heading>
-            <ParagraphSmall>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum fermentum velit ante, ac fringilla nulla pulvinar in.
-              Aenean ut nisi mattis, lobortis purus vel, aliquet ante. In vel
-              viverra lectus. Vivamus a diam faucibus, rutrum quam a, varius
-              felis. Sed pellentesque sodales libero commodo vestibulum.
-              Phasellus convallis gravida tempor. Sed ut bibendum nisl.
-            </ParagraphSmall>
+            <Heading>NextJS with Base Web Template</Heading>
+            <ParagraphLarge>
+              This template comes with Base Web with Styletron component styling package.
+              <br />
+              The template also has the Atomic Architecture file structure.
+            </ParagraphLarge>
             <Button
               onClick={() => alert('click')}
               kind={KIND.primary}
